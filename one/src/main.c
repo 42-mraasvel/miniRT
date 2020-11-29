fb736e7f18acf31630a406561e5df21daa24ad2a
mraasvel <mbraasveldt@gmail.com>
2020-11-29 09:22:09 +0100

    automatic push

--- one/src/main.c
+++ one/src/main.c
@@ -6,10 +6,11 @@
 /*   By: mraasvel <mraasvel@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/28 22:38:47 by mraasvel      #+#    #+#                 */
-/*   Updated: 2020/11/28 22:40:00 by mraasvel      ########   odam.nl         */
+/*   Updated: 2020/11/29 09:20:29 by mraasvel      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "mlx.h"
 
 int	main(void)
@@ -17,6 +18,7 @@ int	main(void)
 	void	*mlx;
 
 	mlx = mlx_init();
-	(void)mlx;
+	free(mlx);
+	// (void)mlx;
 	return (0);
 }
