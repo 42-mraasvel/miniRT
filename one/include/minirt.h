8ae01a25229afc4ef54732473493f3c810870996
mraasvel <mbraasveldt@gmail.com>
2020-12-01 10:43:40 +0100

    automatic push

--- one/include/minirt.h
+++ one/include/minirt.h
@@ -6,19 +6,19 @@
 /*   By: mraasvel <mraasvel@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/30 18:34:09 by mraasvel      #+#    #+#                 */
-/*   Updated: 2020/11/30 21:42:36 by mraasvel      ########   odam.nl         */
+/*   Updated: 2020/12/01 10:02:10 by mraasvel      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
 
-typedef enum	e_errnums
+typedef enum	e_errnum
 {
 	success,
 	malloc_error,
 	write_error
-}				t_errnums;
+}				t_errnum;
 
 typedef struct	s_pair
 {
@@ -36,11 +36,19 @@ typedef struct	s_line
 	t_pair	*coordinates;
 }				t_line;
 
+/*
+** vert_points stores the lowest and highest y of the triangle points.
+** vert_points.x = highest(south in pixel map) y, vert_points.y = lowest(north in pixel map) y
+** hori_points stores lowest and highest x for each y between lowest and highest y.
+** hori_points.x is the rightmost(largest) x, hori_points.y is the leftmost(lowest) x
+*/
+
 typedef struct	s_triangle
 {
 	t_pair	points[3];
 	t_line	lines[3];
-	t_pair	*end_points;
+	t_pair	vert_points;
+	t_pair	*hori_points;
 }				t_triangle;
 
 typedef struct	s_data
--- one/src/draw_triangle.c
+++ one/src/draw_triangle.c
@@ -6,12 +6,78 @@
 /*   By: mraasvel <mraasvel@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/30 21:31:39 by mraasvel      #+#    #+#                 */
-/*   Updated: 2020/11/30 21:49:36 by mraasvel      ########   odam.nl         */
+/*   Updated: 2020/12/01 10:40:00 by mraasvel      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "minirt.h"
+
+static int	ft_max(int a, int b)
+{
+	return (a  > b ? a : b);
+}
+
+static int	ft_min(int a, int b)
+{
+	return (a < b ? a : b);
+}
+
+t_triangle	set_triangle_information(t_triangle triangle)
+{
+
+	triangle.vert_points.x = ft_max(triangle.points[0].y, triangle.points[1].y);
+	triangle.vert_points.x = ft_max(triangle.vert_points.x, triangle.points[2].y);
+	triangle.vert_points.y = ft_min(triangle.points[0].y, triangle.points[1].y);
+	triangle.vert_points.y = ft_min(triangle.vert_points.y, triangle.points[2].y);
+	return (triangle);
+}
+
+int	free_triangle(t_triangle triangle, t_errnum type, int location)
+{
+	if (type == success)
+	{
+		free(triangle.lines[0].coordinates);
+		free(triangle.lines[1].coordinates);
+		free(triangle.lines[2].coordinates);
+		free(triangle.hori_points);
+	}
+	else
+	{
+		if (location > 0)
+			free(triangle.lines[0].coordinates);
+		if (location > 1)
+			free(triangle.lines[1].coordinates);
+		if (location > 2)
+			free(triangle.lines[2].coordinates);
+		if (location > 3)
+			free(triangle.hori_points);
+	}
+	return (type);
+}
+
+int	set_min_max_values(t_pair vert_points, t_pair *hori_points, t_line line)
+{
+	
+}
+
+/*
+** size: y_high - y_low
+*/
+
+int	set_triangle_horizontal_points(t_triangle *triangle)
+{
+	int	height;
+
+	height = triangle->vert_points.x - triangle->vert_points.y + 1;
+	triangle->hori_points = (t_pair*)calloc(height, sizeof(t_pair)); // set to ft_calloc
+	if (triangle->hori_points == NULL)
+		return (malloc_error);
+	set_min_max_values(triangle->vert_points, triangle->hori_points, triangle->lines[0]);
+	set_min_max_values(triangle->vert_points, triangle->hori_points, triangle->lines[1]);
+	set_min_max_values(triangle->vert_points, triangle->hori_points, triangle->lines[2]);
+	return (success);
+}
 
 int	draw_triangle(t_triangle triangle, t_data *img)
 {
@@ -19,23 +85,13 @@ int	draw_triangle(t_triangle triangle, t_data *img)
 	if (triangle.lines[0].coordinates == NULL)
 		return (malloc_error);
 	triangle.lines[1] = generate_line(triangle.points[0].x, triangle.points[0].y, triangle.points[2].x, triangle.points[2].y);
-	if (triangle.lines[0].coordinates == NULL)
-	{
-		free(triangle.lines[0].coordinates);
-		return (malloc_error);
-	}
+	if (triangle.lines[1].coordinates == NULL)
+		return (free_triangle(triangle, malloc_error, 1));
 	triangle.lines[2] = generate_line(triangle.points[1].x, triangle.points[1].y, triangle.points[2].x, triangle.points[2].y);
-	if (triangle.lines[0].coordinates == NULL)
-	{
-		free(triangle.lines[0].coordinates);
-		free(triangle.lines[1].coordinates);
-		return (malloc_error);
-	}
-	draw_line(triangle.points[0], triangle.points[1], img);
-	draw_line(triangle.points[0], triangle.points[2], img);
-	draw_line(triangle.points[1], triangle.points[2], img);
-	free(triangle.lines[0].coordinates);
-	free(triangle.lines[1].coordinates);
-	free(triangle.lines[2].coordinates);
-	return (success);
+	if (triangle.lines[2].coordinates == NULL)
+		return (free_triangle(triangle, malloc_error, 2));
+	triangle = set_triangle_information(triangle);
+	if (set_triangle_horizontal_points(&triangle) == malloc_error)
+		return (free_triangle(triangle, malloc_error, 3));
+	return (free_triangle(triangle, success, 0));
 }
