a16c80efa1e7c92784ea76f553f7b18d847e6cb1
mraasvel <mbraasveldt@gmail.com>
2021-01-07 17:18:48 +0100

    automatic push

--- linux/program/src/intersections/intersection.c
+++ linux/program/src/intersections/intersection.c
@@ -6,13 +6,13 @@
 /*   By: mraasvel <mraasvel@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/29 22:42:44 by mraasvel      #+#    #+#                 */
-/*   Updated: 2020/12/31 08:48:12 by mraasvel      ########   odam.nl         */
+/*   Updated: 2021/01/07 13:30:03 by mraasvel      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 #include "prototypes.h"
-#include <headers.h> // rm
+#include "headers.h" // rm
 
 /*
 ** Goal: Find nearest POSITIVE intersection
