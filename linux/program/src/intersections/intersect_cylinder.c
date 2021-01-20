667f31741dbf705aea2bce106c33d8fe3f5b2858
mraasvel <mbraasveldt@gmail.com>
2021-01-20 17:36:09 +0100

    automatic push

--- linux/program/src/intersections/intersect_cylinder.c
+++ linux/program/src/intersections/intersect_cylinder.c
@@ -6,15 +6,52 @@
 /*   By: mraasvel <mraasvel@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/01/20 13:17:49 by mraasvel      #+#    #+#                 */
-/*   Updated: 2021/01/20 13:21:19 by mraasvel      ########   odam.nl         */
+/*   Updated: 2021/01/20 17:32:52 by mraasvel      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers.h"
 
+static double	solve_for_t(double b, double discriminant, int sign)
+{
+	return ((-b + sign * sqrt(discriminant)) / 2);
+}
+
+static double	get_nearest_t(double b, double discriminant)
+{
+	double	t1;
+	double	t2;
+
+	t1 = solve_for_t(b, discriminant, 1);
+	if (discriminant == 0)
+		return (t1);
+	t2 = solve_for_t(b, discriminant, -1);
+	if (t1 < 0 || t2 < 0)
+		return (ft_fmax(t1, t2));
+	return (ft_fmin(t1, t2));
+}
+
 static double	intersect_cylinder(t_vec3 origin, t_vec3 direction, t_cylinder cylinder)
 {
-	return (-1);
+	double	a;
+	double	b;
+	double	c;
+	t_vec3 tmp;
+	t_vec3 cip;
+
+	a = vec_sub(direction, vec_scalar(cylinder.orientation, vec_dot(direction, cylinder.orientation)));
+	a = vec_dot(a, a);
+	cip = vec_sub(origin, cylinder.position)
+	tmp = vec_sub(direction, vec_dot(vec_dot(vec_dot(direction, cylinder.orientation), cylinder.orientation), cip));
+	tmp = vec_sub(tmp, vec_dot(vec_dot(cip, cylinder.orientation), cylinder.orientation));
+	b = vec_scalar(tmp, 2);
+	tmp = vec_sub(cip, vec_dot(vec_dot(cip, cylinder.orientation), cylinder.orientation));
+	c = vec_dot(tmp, tmp) - (cylinder.diameter / 2) * (cylinder.diameter/2)
+
+	double discriminant = (b * b) - 4 * c;
+	double t = get_nearest_t(b, discriminant);
+
+	return (t);
 }
 
 t_vec3	calculate_cylinder_normal(t_cylinder cylinder, t_vec3 intersection_point)
