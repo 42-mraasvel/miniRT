e551232730ccebac40e6d7827903e174e03126ed
mraasvel <mbraasveldt@gmail.com>
2021-01-30 14:21:59 +0100

    automatic push

--- linux/program/src/rendering/render_image.c
+++ linux/program/src/rendering/render_image.c
@@ -6,7 +6,7 @@
 /*   By: mraasvel <mraasvel@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/29 11:45:31 by mraasvel      #+#    #+#                 */
-/*   Updated: 2021/01/29 14:04:53 by mraasvel      ########   odam.nl         */
+/*   Updated: 2021/01/30 14:21:52 by mraasvel      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,6 @@ void	print_camera_info(t_camera c)
 	printf("Orientation:\n");
 	printf("<%.2f %.2f %.2f>\n", c.orientation.x, c.orientation.y, c.orientation.z);
 	printf("\n");
-}
-
-t_vec3	calculate_image_start(t_scene *scene, t_space camera_space, t_camera camera)
-{
-	t_vec3	start;
-	double	image_distance;
-	double	x_half;
-	double	y_half;
-
-	x_half = (double)scene->resolution.x / 2.0 - 0.5;
-	y_half = (double)scene->resolution.y / 2.0 - 0.5;
-	image_distance = x_half / (tan(deg_to_rad(camera.fov / 2)));
-	start = vec_add(camera_space.origin, vec_scalar(camera_space.base_z, image_distance));
-	start = vec_sub(start, vec_scalar(camera_space.base_x, x_half));
-	start = vec_add(start, vec_scalar(camera_space.base_y, y_half));
-	return (start);
 }
 
 t_col	compute_ambient(t_ambient ambient)
@@ -116,6 +100,44 @@ int	ray_tracing(t_camera camera, t_vec3 direction, t_scene *scene, t_col *color)
 	return (success);
 }
 
+t_vec3	calculate_image_start(t_scene *scene, t_space camera_space, t_camera camera)
+{
+	t_vec3	start;
+	double	image_distance;
+	double	x_half;
+	double	y_half;
+
+	x_half = (double)scene->resolution.x / 2.0 - 0.5;
+	y_half = (double)scene->resolution.y / 2.0 - 0.5;
+	image_distance = x_half / (tan(deg_to_rad(camera.fov / 2)));
+	start = vec_add(camera_space.origin, vec_scalar(camera_space.base_z, image_distance));
+	start = vec_sub(start, vec_scalar(camera_space.base_x, x_half));
+	start = vec_add(start, vec_scalar(camera_space.base_y, y_half));
+	return (start);
+}
+
+t_vec3	compute_pixel_position(int x, int y, t_space camera_space, t_vec3 z_dir)
+{
+	t_vec3	x_dir;
+	t_vec3	y_dir;
+	t_vec3	position;
+
+	x_dir = vec_scalar(camera_space.base_x, x);
+	y_dir = vec_scalar(camera_space.base_x, y);
+	position = vec_add(camera_space.origin, vec_add(vec_add(x_dir, y_dir), z_dir));
+	return (position);
+}
+
+t_vec3	get_zdir(t_scene *scene, t_space camera_space, t_camera camera)
+{
+	double	x_half;
+	double	image_distance;
+
+	x_half = (double)scene->resolution.x / 2.0 - 0.5;
+	image_distance = x_half / (tan(deg_to_rad(camera.fov / 2)));
+	return (vec_scalar(camera_space.base_z, image_distance));
+}
+
 /*
 ** Raytracing Algorithm:
 ** 1. For Each Pixel
@@ -142,19 +164,21 @@ int	render_image(t_scene *scene, t_img *img, t_camera camera)
 	i = 0;
 	camera_space = new_coordinate_space(camera.position, camera.orientation);
 	start = calculate_image_start(scene, camera_space, camera);
+	t_vec3 z_dir = get_zdir(scene, camera_space, camera);
 	while (i < scene->resolution.y)
 	{
 		j = 0;
-		pixel_position = start;
+		// pixel_position = start;
 		while (j < scene->resolution.x)
 		{
+			pixel_position = compute_pixel_position(j, i, camera_space, z_dir);
 			if (ray_tracing(camera, vec_dir(camera.position, pixel_position), scene, &color) != success)
 				return (render_error);
 			ft_pixel_put(*img, j, i, color);
-			pixel_position = vec_add(pixel_position, camera_space.base_x);
+			// pixel_position = vec_add(pixel_position, camera_space.base_x);
 			j++;
 		}
-		start = vec_sub(start, camera_space.base_y);
+		// start = vec_sub(start, camera_space.base_y);
 		i++;
 	}
 	return (success);
